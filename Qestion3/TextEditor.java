2f2d72b15474b517e65f0146bb2aed650c17d155
User <sajibuddin729@gamil.com>
2024-05-26 10:39:17 +0600

    this is first commite for this code

---
+++ Qestion3/TextEditor.java
@@ -0,0 +1,21 @@
+package Qestion3;
+
+public class TextEditor {
+    public static void main(String[] args) {
+        try {
+            Text myText = new Text("I am a DP");
+            System.out.println(myText.getDescription());
+
+            Text boldText = new BoldText(myText);
+            System.out.println(boldText.getDescription());
+
+            Text italicText = new ItalicText(boldText);
+            System.out.println(italicText.getDescription());
+
+            Text underlineText = new UnderlineText(italicText);
+            System.out.println(underlineText.getDescription());
+        } catch (Exception e) {
+            System.out.println("Error in text processing: " + e.getMessage());
+        }
+    }
+}
