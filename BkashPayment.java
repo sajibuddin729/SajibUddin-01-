ad9a670250a7b8e23eb08ccc8f0294787091522a
User <sajibuddin729@gamil.com>
2024-05-26 10:14:38 +0600

    this is 4th commit

--- BkashPayment.java
+++ BkashPayment.java
@@ -3,7 +3,7 @@ public class BkashPayment implements PaymentStrategy {
     @Override
     public boolean pay(double amount) {
         System.out.println("Making payment using Bkash.");
-        // Do something 
+        // Do something bout it
         return true;
     }
 }
